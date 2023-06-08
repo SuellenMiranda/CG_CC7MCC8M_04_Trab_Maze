42e117fa1b0743dba979bf231e1e4639995ee103
Suh <suellen.org@gmail.com>
2023-06-08 14:48:24 -0300

    Add files via upload

---
+++ Assets/Scripts/MoveAround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAround : MonoBehaviour
+{
+    public CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("LookAround");
+
+        Vector3 moveCal = transform.right * x + transform.forward * z;
+
+        controller.Move(moveCal * 70 * Time.deltaTime);
+
+        GetComponent<Transform>().Rotate(Vector3.up * rotation * 50);
+    }
+}
